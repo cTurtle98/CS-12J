425ed6decd5649dba467bd76c2849a5aa298b08a
Ciaran Farley <ciaran@cturtle98.com>
2019-09-25 18:03:22 -0700

    Update DisplacementGivenVelocity.java

--- as02/DisplacementGivenVelocity.java
+++ as02/DisplacementGivenVelocity.java
@@ -1,25 +1,25 @@
-     1	import java.util.Scanner;
-     2	
-     3	/**
-     4	  * Ciaran Farley for CS-12J
-     5	  * Displacement Given Time
-     6	  * https://jeff.cis.cabrillo.edu/classes/cs12js19/assignments/assignment_02
-     7	**/
-     8	public class DisplacementGivenVelocity {
-     9	  /** idk why I need this **/
-    10	  public static void main(String[] args) {
-    11	    Scanner input = new Scanner(System.in);
-    12	    
-    13	    System.out.println("whats the time? ");
-    14	    double time = input.nextDouble();
-    15	    
-    16	    System.out.println("Whats the initial velocity");
-    17	    double velocity0 = input.nextDouble();
-    18	    
-    19	    System.out.println("whats the final velocity");
-    20	    double velocityFinal = input.nextDouble();
-    21	    
-    22	    double displacement = time * ( ( velocity0 + velocityFinal ) / 2 );
-    23	    System.out.printf("%.2f", displacement);
-    24	  }
-    25	}
+import java.util.Scanner;
+
+/**
+  * Ciaran Farley for CS-12J
+  * Displacement Given Time
+  * https://jeff.cis.cabrillo.edu/classes/cs12js19/assignments/assignment_02
+**/
+public class DisplacementGivenVelocity {
+  /** idk why I need this **/
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("whats the time? ");
+    double time = input.nextDouble();
+    
+    System.out.println("Whats the initial velocity");
+    double velocity0 = input.nextDouble();
+    
+    System.out.println("whats the final velocity");
+    double velocityFinal = input.nextDouble();
+    
+    double displacement = time * ( ( velocity0 + velocityFinal ) / 2 );
+    System.out.printf("%.2f", displacement);
+  }
+}
